70fc6129fc8af4c643a1cdd9561556332f55eba3
wakebro <wakebro119@naver.com>
2026-03-25 12:12:54 +0900

    fix forever

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # Navigate to application directory
-cd /home/ec2-user/app || exit
+cd /home/ec2-user/app || exit 1
+npm install
 
 # Start the server using forever
 forever start -o out_resize.log -e err_resize.log src/server.js
--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-forever stopall
+# forever stopall
+cd /home/ec2-user/app || exit 1
+npx forever stopall || true
